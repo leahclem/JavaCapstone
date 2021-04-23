c8b703091b2d772c1cddf495153384368a48e31d
willm <willm@DESKTOP-IDBPVQ3>
2021-04-23 14:10:21 -0400

    update

--- s21_Meerkats/s21_Meerkat_Project/SQLMethods.java
+++ s21_Meerkats/s21_Meerkat_Project/SQLMethods.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class SimpleConnectToMySQL {
+public class SQLMethods {
 	static Connection con = null;
 	static Statement stmt = null;
 
