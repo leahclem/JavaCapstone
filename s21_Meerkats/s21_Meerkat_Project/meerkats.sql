8cad9946c2d90e34cfc02c5f80b7eb0443b1cbe2
willm <willm@DESKTOP-IDBPVQ3>
2021-04-27 14:35:32 -0400

    most recent meerkats.sql implementation

--- s21_Meerkats/s21_Meerkat_Project/meerkats.sql
+++ s21_Meerkats/s21_Meerkat_Project/meerkats.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2021 at 12:14 AM
+-- Generation Time: Apr 27, 2021 at 08:30 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.3.27
 
